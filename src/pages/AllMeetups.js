9537f3610a3f470757a2648f3a2bb8f323af9ed5
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-22 10:49:51 +0800

    Getting Started with Fetching Data

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,31 +1,30 @@
+import { useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-    {
-      id: 'm1',
-      title: 'This is a first meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-    {
-      id: 'm2',
-      title: 'This is a second meetup',
-      image:
-        'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-      address: 'Meetupstreet 5, 12345 Meetup City',
-      description:
-        'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-    },
-];
-
 function AllMeetupsPage() {
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+    // Will cause an infinite loop because we keep fetching data whenever we send a request to our Firebase API.
+    // Remember that whenever a state changes, React re-renders the page to reflect updated changes to the state, and so the fetch() function will be called over and over again, making lots of requests to the Firebase API.
+    fetch('https://react-tut-d3e68-default-rtdb.firebaseio.com/meetups.json')
+        .then(response => {
+            return response.json(); // Read the data from Firebase API
+        }).then(data => {
+            setIsLoading(false);
+            setLoadedMeetups(data); // Once data is retrieved, store this data in the loadedMeetups state
+        });
+
+    if (isLoading) {
+        return (
+            <section><p>Loading...</p></section>
+        );
+    }
+
     return (
         <section>
             <h1>All Meetups</h1>
-            <MeetupList meetups={DUMMY_DATA} />
+            <MeetupList meetups={loadedMeetups} />
         </section>
     );
 }
