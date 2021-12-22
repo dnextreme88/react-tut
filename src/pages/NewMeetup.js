a046d98a87bd7321c7cb6af24c16a9d422b73623
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-22 10:24:21 +0800

    Sending a Post Request

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -2,7 +2,16 @@ import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
     function addMeetupHandler(meetupData) {
-
+        // For sending an HTTP request similar to axios library
+        // By default, fetch(url) sends a GET request unless you pass a second argument, which is an object
+        fetch(
+            'https://react-tut-d3e68-default-rtdb.firebaseio.com/meetups.json',
+            { // body key must be an object in JSON format so use JSON.stringify()
+                method: 'POST',
+                body: JSON.stringify(meetupData),
+                headers: { 'Content-Type': 'application/json' }
+            }
+        );
     }
 
     return (
