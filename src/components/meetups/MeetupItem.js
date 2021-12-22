239ad8d2a6fe6f431ca0d06447c62e260787b6e3
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-23 04:31:34 +0800

    Using Context in Components

--- src/components/meetups/MeetupItem.js
+++ src/components/meetups/MeetupItem.js
@@ -1,9 +1,30 @@
+import { useContext } from 'react';
 import Card from '../ui/Card';
+import FavoritesContext from '../../store/favorites-context';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+    // Gets the prop value={context} from context object located in '../../store/favorites-context'. The key properties of function names point to their respective functions, which can then be called by useContext(FavoritesContext) as well as other state values such as favorites or totalFavorites.
+    const favoritesCtx = useContext(FavoritesContext);
+    const itemIsFavorite = favoritesCtx.itemIsFavorite(props.id);
+
+    function toggleFavoriteStatusHandler() {
+        if (itemIsFavorite) {
+            favoritesCtx.removeFavorite(props.id);
+        } else {
+            favoritesCtx.addFavorite({
+                id: props.id,
+                title: props.title,
+                description: props.description,
+                image: props.image,
+                address: props.address,
+            });
+        }
+    }
+
     return (
         <li className={classes.item}>
+            <p>Total favorites count: {favoritesCtx.totalFavorites}</p>
             <Card>
                 <div className={classes.image}>
                     <img src={props.image} alt={props.title} />
@@ -14,7 +35,7 @@ function MeetupItem(props) {
                     <p>{props.description}</p>
                 </div>
                 <div className={classes.actions}>
-                    <button>To Favorites</button>
+                    <button onClick={toggleFavoriteStatusHandler}>{itemIsFavorite ? 'Remove from Favorites' : 'To Favorites'}</button>
                 </div>
             </Card>
         </li>
--- src/store/favorites-context.js
+++ src/store/favorites-context.js
@@ -3,10 +3,13 @@ import { createContext, useState } from "react";
 const FavoritesContext = createContext({
     favorites: [],
     totalFavorites: 0,
+    addFavorite: (favoriteMeetup) => {},
+    removeFavorite: (meetupId) => {},
+    itemIsFavorite: (meetupId) => {},
 });
 
 // Its job is to provide this context to all the components that are interested in listening to the values so all the components that need values from the context and this function will also be responsible for updating the context values
-function FavoritesContextProvider(props) {
+export function FavoritesContextProvider(props) {
     const [userFavorites, setUserFavorites] = useState([]);
 
     function addFavoriteHandler(favoriteMeetup) {
@@ -28,6 +31,9 @@ function FavoritesContextProvider(props) {
     const context = {
         favorites: userFavorites,
         totalFavorites: userFavorites.length,
+        addFavorite: addFavoriteHandler,
+        removeFavorite: removeFavoriteHandler,
+        itemIsFavorite: itemIsFavoriteHandler,
     };
 
     // This Provider component needs to be wrapped around all components that are interested in interacting with the FavoritesContext context
@@ -35,3 +41,5 @@ function FavoritesContextProvider(props) {
         {props.children}
     </FavoritesContext.Provider>
 }
+
+export default FavoritesContext;
