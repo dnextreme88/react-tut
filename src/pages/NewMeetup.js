69dc857c8fa2f4cbdd0740813e6ea300a97da91a
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-22 10:34:02 +0800

    Navigating Programatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,6 +1,9 @@
+import { useHistory } from 'react-router-dom';
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 
 function NewMeetupPage() {
+    const history = useHistory();
+
     function addMeetupHandler(meetupData) {
         // For sending an HTTP request similar to axios library
         // By default, fetch(url) sends a GET request unless you pass a second argument, which is an object
@@ -11,7 +14,9 @@ function NewMeetupPage() {
                 body: JSON.stringify(meetupData),
                 headers: { 'Content-Type': 'application/json' }
             }
-        );
+        ).then(() => {
+            history.replace('/'); // Navigate away but using the back button will not go back to the previous page, which is the form
+        });
     }
 
     return (
