4b900335a780f4bcc7a85f5de77b0185661302e1
Jeanne Kevin T. Decena <storm_iz_here@yahoo.com>
2021-12-23 02:24:44 +0800

    useEffect

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
 function AllMeetupsPage() {
     const [isLoading, setIsLoading] = useState(true);
     const [loadedMeetups, setLoadedMeetups] = useState([]);
 
-    // Will cause an infinite loop because we keep fetching data whenever we send a request to our Firebase API.
-    // Remember that whenever a state changes, React re-renders the page to reflect updated changes to the state, and so the fetch() function will be called over and over again, making lots of requests to the Firebase API.
-    fetch('https://react-tut-d3e68-default-rtdb.firebaseio.com/meetups.json')
+    // Only run the HTTP GET request once when the component renders for the 1st time. This is to avoid the infinite loop if we place the fetch() function outside of useEffect() function.
+    // Remember that whenever a state changes, React re-renders the page to reflect updated changes to the state, and so the fetch() function should be inside this function so that it only makes a request once to the Firebase API.
+    useEffect(() => {
+        fetch('https://react-tut-d3e68-default-rtdb.firebaseio.com/meetups.json')
         .then(response => {
             return response.json(); // Read the data from Firebase API
         }).then(data => {
+            const meetups = [];
+            
+            for (const key in data) {
+                const meetup = {
+                    id: key,
+                    ...data[key]
+                };
+                meetups.push(meetup);
+            }
             setIsLoading(false);
-            setLoadedMeetups(data); // Once data is retrieved, store this data in the loadedMeetups state
+            setLoadedMeetups(meetups); // Once data is retrieved, store this data in the loadedMeetups state
         });
+    }, []);
 
     if (isLoading) {
         return (
